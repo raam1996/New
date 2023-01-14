24790a49d58ef7441fc5d37c2015cde8b42aa261
raam1996 <mukeshraam1996@gmail.com>
2023-01-14 07:06:26 +0530

    First commit

---
+++ src/login/GeographyInfo.java
@@ -0,0 +1,18 @@
+package login;
+
+public class GeographyInfo {
+	public void tamilLanguage() {
+		System.out.println("TAMIL");
+	}
+	public void hindiLanguage() {
+		// TODO Auto-generated method stub
+		System.out.println("HINDI");
+
+	}
+	public void englishLanguage() {
+		// TODO Auto-generated method stub
+		System.out.println("ENGLISH");
+
+	}
+
+}
---
+++ src/org/country/CountryInfo.java
@@ -0,0 +1,29 @@
+package org.country;
+
+public class CountryInfo extends GographyInfo {
+	private void northIndia() {
+		// TODO Auto-generated method stub
+		System.out.println("delhi");
+
+	}
+	private void centralIndia() {
+		// TODO Auto-generated method stub
+		System.out.println("bhopal");
+
+	}
+	private void southIndia() {
+		// TODO Auto-generated method stub
+		System.out.println("chennai");
+
+	}
+	public static void main(String[] args) {
+		CountryInfo a =new CountryInfo();
+		a.northIndia();
+		a.centralIndia();
+		a.southIndia();
+		a.tamilanguage();
+		
+	}
+
+
+}
